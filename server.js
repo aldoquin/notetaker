6ec2f1ca415ddc01b99de8cb13ca1617373d4b19
Aldo Quintero <aldoq95@gmail.com>
2021-08-05 21:57:34 -0700

    pushing

--- server.js
+++ server.js
@@ -3,12 +3,12 @@ const app = express();
 const path = require('path');
 const fs = require('fs')
 const note = require('./db/notes');
-const db = require('../db.json');
+const db = require('./db.json');
 // const characters = require('./characters');
 // const logger = require ('./logger')
 
 const PORT = process.env.PORT || 3001
-notes = JSON.parse(fs.readFileSync('../db.json'));
+notes = JSON.parse(fs.readFileSync('./db.json'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
